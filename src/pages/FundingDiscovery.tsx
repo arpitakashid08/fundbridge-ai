b5dd4ffee63c865cb9efeb0d871b54581d4db543
Arpita Kashid <arpitakashid@gmail.com>
2026-08-16 16:21:04 +0530

    Fix production API URL

--- src/pages/FundingDiscovery.tsx
+++ src/pages/FundingDiscovery.tsx
@@ -1,6 +1,6 @@
 import { useEffect, useMemo, useState } from "react";
 
-const API_URL = "http://127.0.0.1:5050";
+const API_URL = import.meta.env.VITE_API_URL || "http://127.0.0.1:5050";
 
 type Opportunity = {
   id: string;
